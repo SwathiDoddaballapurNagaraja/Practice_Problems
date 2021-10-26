9ca78141a003eddf3faa0c3961b8592633780f02
Swathi Doddaballapur Nagaraja <swathimanag95@gmail.com>
2021-10-26 17:08:14 -0400

    initial commit

---
+++ class Candies.java
@@ -0,0 +1,20 @@
+class Candies {
+    public int distributeCandies(int[] candyType) {
+        
+        HashSet<Integer> unique = new HashSet<Integer>();
+        
+        for(int i=0; i<candyType.length; i++){
+            if(unique.size() < candyType.length/2){
+                unique.add(candyType[i]);
+            }
+            
+        }      
+        return unique.size();
+    }
+
+    public static void main(str[] args){
+        int [] candyType = [1,1,2,2,3,3];
+        int result = distributeCandies(candyType);
+        System.out.println(result);
+    }
+}
